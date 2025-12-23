e63a86cd7e6ece3a74b2110b1bc24d6ac80b0d4d
Hector Corona <coronahe95@gmail.com>
2025-12-22 23:31:06 -0800

    Created Account cpp and hpp for PIN and Balance to live in, instead of User

---
+++ account.cpp
@@ -0,0 +1,12 @@
+#ifndef ACCOUNT_HPP
+#define ACCOUNT_HPP
+
+#include <iostream>
+
+//* This class gets the information from User, where PIN and balance will live, hopefully
+class Account {
+    private: 
+
+};
+
+#endif
