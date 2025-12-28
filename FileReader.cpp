2606ba776cdca49f3275a5fb84191bd7e1318284
Hector Corona <hcorona9@calstatela.edu>
2025-12-28 10:58:58 -0800

    adjusted main with file reader logic

--- FileReader.cpp
+++ FileReader.cpp
@@ -1,17 +1,17 @@
-#ifndef FILEREADER_HPP
-#define FILEREADER_HPP
-
-#include <iostream>
-#include <fstream>
-
-class FileReader {
-    private:
-        std::string fileName;
-
-    public:
-        FileReader();
-        FileReader(std::string filename);
-};
-
-
-#endif
+void File::fileRead(std::string fileName) {
+    std::ifstream inputFile(fileName);
+    if(!inputFile) {
+        std::cout << "Was not able to open file: " << fileName << std::endl;
+        return;
+    }
+    std::string firstName, lastName, fullName, accNum;
+    int pin;
+    float balance;
+    
+    while(inputFile >> accNum >> firstName >> lastName >> pin >> balance) {
+        fullName = firstName + lastName;
+        User user(accNum, firstName, lastName, pin, balance);
+        userMap[accNum] = user;
+    }
+    inputFile.close();
+}
