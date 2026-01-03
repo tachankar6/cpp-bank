a80096120167a50b33b2f8017864405309fbf3c9
Hector Corona <coronahe95@gmail.com>
2026-01-03 10:07:27 -0800

    feature update

--- FileReader.cpp
+++ FileReader.cpp
@@ -3,24 +3,35 @@
 #include "FileReader.hpp"
 #include "User.hpp"
 
-void File::File() {
+FileReader::FileReader() {
     std::string filename;
+    std::string account, first, last;
+    int pin, balance;
     std::cout << "What file would you like to open?\n";
     std::getline(std::cin, fileName);
+
+    std::ifstream textFile(fileName);
+    if(!textFile) {
+        std::cout << "File name " << fileName << " could not be opened, please select another file\n";
+        do {
+            std::cout << "Unable to open that file name\nPlease select another";
+            std::getline(std::cin, fileName);
+        } while (!textFile);
+    }
+    else if (textFile.is_open()) { // condition for file being opened
+        while (textFile >> account >> first >> last >> pin >> balance) {
+            std::string fullName = first +  " " + last;
+            User users(account, first, last, pin, balance); 
+        }
+    }
+
 }
 
 
 
 
 
-
-
-
-
-
-
-
-void File::fileRead(std::string fileName) {
+FileReader::FileReader(std::string fileName) {
     std::ifstream inputFile(fileName);
     if(!inputFile) {
         std::cout << "Was not able to open file: " << fileName << std::endl;
